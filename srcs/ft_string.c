0bde055b7614e332139adbaa66f2cd6ebdfc5e4d
UriVentura <uriventura97@gmail.com>
2021-08-04 20:13:41 +0200

    errores itoa

--- srcs/ft_string.c
+++ srcs/ft_string.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_string.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oventura <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/04 19:45:05 by oventura          #+#    #+#             */
+/*   Updated: 2021/08/04 19:45:07 by oventura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "../includes/ft_printf.h"
 
@@ -5,13 +17,15 @@ int     ft_string(va_list args)
 {
     t_types type;
     int     i;
+    char    *res;
 
     type.string = va_arg(args, char *);
     i = 0;
-    if (!type.string)
+    res = type.string;
+    if (!res)
         return (write(1, "(null)", 6));
-    while(type.string[i] != '\0')
-        ft_putchar(type.string[i++]);
-    ft_clearstr(&type.string);
+    while(res[i] != '\0')
+        ft_putchar(res[i++]);
+    ft_clearstr(&res);
     return (i);
 }
